6eb2bac01d2ca3de30d000f30759e0a002cf5771
soongon <soongon@gmail.com>
2024-10-21 15:54:54 +0900

    add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class StopWatch extends StatefulWidget {
 class _StopWatchState extends State<StopWatch> {
 
   int seconds = 0;
+  bool isTicking = true;
   late Timer timer;
 
   @override
@@ -42,11 +43,23 @@ class _StopWatchState extends State<StopWatch> {
   }
 
   void _onTick(Timer time) {
-    if (mounted) {
+    if (mounted && isTicking) {
       setState(() {
         ++seconds;
       });
     }
+  }
+
+  void _startTimer() {
+    setState(() {
+      isTicking = true;
+    });
+  }
+
+  void _stopTimer() {
+    setState(() {
+      isTicking = false;
+    });
   }
 
   String _secondsText() =>seconds == 1 ? 'second' : 'seconds';
@@ -57,11 +70,38 @@ class _StopWatchState extends State<StopWatch> {
       appBar: AppBar(
         title: const Text('Stopwatch'),
       ),
-      body: Center(
-        child: Text(
-          '$seconds ${_secondsText()}',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$seconds ${_secondsText()}',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.green),
+                  foregroundColor: WidgetStateProperty.all(Colors.white),
+                ),
+                onPressed: _startTimer,
+                child: Text('Start')
+              ),
+              SizedBox(width: 20,),
+              ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.red),
+                    foregroundColor: WidgetStateProperty.all(Colors.white),
+                  ),
+                onPressed: _stopTimer,
+                child: Text('Stop')
+              ),
+
+            ],
+          )
+        ],
       )
     );
   }
